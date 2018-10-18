bc07504b40c2b66add0fa2d14163aab5d2881924
zhoupeng <zhoupeng@xjgreat.com>
2018-10-18 18:16:42 +0800

    o

--- ghpage.sh
+++ ghpage.sh
@@ -1,8 +1,9 @@
 rm -rf dist;
 npm run build;
-git checkout gh-pages;
-git rm demo.*;
 cp -r dist/* .;
+git checkout gh-pages;
+git rm --cached demo.*;
+
 git add index.html *.png *.css demo.*;
 git commit -m 'update page';
 git push
