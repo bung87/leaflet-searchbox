6b0e186d384c0c7fc0691ba15011b614a0865726
zhoupeng <zhoupeng@xjgreat.com>
2018-10-18 17:45:41 +0800

    Revert "o"
    This reverts commit c4397dc3d1476e6a9a2a0fa126e5685fb62a14a1.

--- ghpage.sh
+++ ghpage.sh
@@ -1,9 +1,8 @@
 rm -rf dist;
 npm run build;
-cp -r dist/* .;
 git checkout gh-pages;
 git rm demo.*;
-
+cp -r dist/* .;
 git add index.html *.png *.css demo.*;
 git commit -m 'update page';
 git push
